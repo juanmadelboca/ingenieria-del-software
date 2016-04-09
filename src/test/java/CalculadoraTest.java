9b455c78a6aeb7a11ebad0ddda36abf79d193dbc
Franco <francodi65@gmail.com>
2016-04-08 22:22:03 -0300

    Nuevos unit test funcionando

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -2,35 +2,92 @@ package test.java;
 import main.java.*;
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class CalculadoraTest {
 
 	@Test
-	public void suma() {
-		Calculadora c=new Calculadora(1,1,"+");
-		c.run();
-		assertTrue(c.resultado==2);
+	public void sumaSimple() {
+		LinkedList<String> cola= new LinkedList<String>();
+		cola.add("2");
+		cola.add("+");
+		cola.add("2");
+		Calculadora c=new Calculadora(cola);
+		assertTrue(c.getResultado()==4);
 	}
 	
 	@Test
-	public void restaTest() {
-		Calculadora c=new Calculadora(2,2,"-");
-		c.run();
-		assertTrue(c.resultado==0);
+	public void restaSimple() {
+		LinkedList<String> cola= new LinkedList<String>();
+		cola.add("2");
+		cola.add("-");
+		cola.add("2");
+		Calculadora c=new Calculadora(cola);
+		assertTrue(c.getResultado()==0);
 	}
 	@Test
-	public void sumaFranco() {
-		Calculadora c=new Calculadora(2,2,"+");
-		c.run();
-		assertTrue(c.resultado==4);
+	public void sumaCompleja() {
+		LinkedList<String> cola= new LinkedList<String>();
+		cola.add("150");
+		cola.add("+");
+		cola.add("252");
+		cola.add("+");
+		cola.add("120");
+		cola.add("+");
+		cola.add("5");
+		cola.add("+");
+		cola.add("15");
+		cola.add("+");
+		cola.add("330");
+		Calculadora c=new Calculadora(cola);
+		assertTrue(c.getResultado()==872);
 	}
 	@Test
-	public void sumaJuanma() {
-		Calculadora c=new Calculadora(3,2,"+");
-		c.run();
-		assertTrue(c.resultado==5);
+	public void restaCompleja() {
+		LinkedList<String> cola= new LinkedList<String>();
+		cola.add("150");
+		cola.add("-");
+		cola.add("252");
+		cola.add("-");
+		cola.add("120");
+		cola.add("-");
+		cola.add("5");
+		cola.add("-");
+		cola.add("15");
+		cola.add("-");
+		cola.add("330");
+		Calculadora c=new Calculadora(cola);
+		assertTrue(c.getResultado()==-572);
 	}
-	
+	@Test
+	public void sumRestSimple() {
+		LinkedList<String> cola= new LinkedList<String>();
+		cola.add("4");
+		cola.add("+");
+		cola.add("2");
+		cola.add("-");
+		cola.add("2");
+		Calculadora c=new Calculadora(cola);
+		assertTrue(c.getResultado()==4);
+	}
+	@Test
+	public void sumRestCompleja() {
+		LinkedList<String> cola= new LinkedList<String>();
+		cola.add("150");
+		cola.add("+");
+		cola.add("252");
+		cola.add("-");
+		cola.add("120");
+		cola.add("+");
+		cola.add("5");
+		cola.add("-");
+		cola.add("15");
+		cola.add("-");
+		cola.add("330");
+		Calculadora c=new Calculadora(cola);
+		assertTrue(c.getResultado()==-58);
+	}
 
 }
